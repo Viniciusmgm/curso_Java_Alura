a256ae78e6917d514671e025feaed6829b0ba8cc
Viniciusmgm <vinicius.mirandagm@gmail.com>
2022-02-08 09:26:28 -0300

    finalização dos primeiros passos

---
+++ sintaxe_variaveis_e_fluxo/src/sintaxe_variaveis_e_fluxo/TestaWhile.java
@@ -0,0 +1,13 @@
+package sintaxe_variaveis_e_fluxo;
+
+public class TestaWhile {
+	public static void main(String[] args) {
+		int c = 0;
+		while(c <= 10) {
+			System.out.println(c);
+			//c = c + 1
+			//c += 1
+			c++;
+		}
+	}
+}
