4e2ef7556588c6d48e6dd0442c56496b42c88bb1
Viniciusmgm <vinicius.mirandagm@gmail.com>
2022-03-16 13:55:40 -0300

    organizando algumas classes com pacotes

--- bytebank_herdado_conta/src/br/com/bytebank/banco/teste/TesteTributaveis.java
+++ bytebank_herdado_conta/src/br/com/bytebank/banco/teste/TesteTributaveis.java
@@ -1,8 +1,13 @@
+package br.com.bytebank.banco.teste;
+
+import br.com.bytebank.banco.modelo.CalculadorDeImposto;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.SeguroDeVida;
 
 public class TesteTributaveis {
 
 	public static void main(String[] args) {
-		
+		//Full Qualified Name FQN
 		ContaCorrente cc = new ContaCorrente();
 		cc.deposita(100.0);
 		
