7ce11c98af9c77cd0d36e7aa4dcfb0b9e72d5d4c
Viniciusmgm <vinicius.mirandagm@gmail.com>
2022-03-25 09:57:47 -0300

    guardando referencias

--- bytebank_herdado_conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
+++ bytebank_herdado_conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
@@ -1,16 +1,25 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 		
-		ContaCorrente[] contas = new ContaCorrente[5]; 
+		Object[] referencias = new ContaCorrente[5]; 
 		ContaCorrente cc1 = new ContaCorrente(22, 11);
+		ContaPoupanca cp2 = new ContaPoupanca(22, 22);
+		Cliente cliente = new Cliente();
 		
-		contas[0] = cc1;
+		referencias[0] = cc1;
+		referencias[1] = cp2;
+		referencias[2] = cliente;
 		
-		System.out.println(contas[0].getNumero());
+		ContaPoupanca ref = (ContaPoupanca) referencias[1];
+//		System.out.println(((Conta) contas[0]).getNumero());
+		System.out.println(ref.getNumero());
 	}
 }
