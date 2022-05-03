3a84fab4d42ca379adf6c3c8ce80de22c3e53e4f
Viniciusmgm <vinicius.mirandagm@gmail.com>
2022-05-03 09:26:48 -0300

    escrita com java.io

---
+++ java_io/src/br/com/alura/java/io/teste/TesteEntradaTeclado.java
@@ -0,0 +1,44 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+public class TesteEntradaTeclado {
+
+	public static void main(String[] args) throws IOException {
+
+		//Fluxo de Entrada com Arquivo
+		InputStream fis = System.in;
+		Reader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+		
+		//Fluxo de Saida com Arquivo
+		//OutputStream fos = new FileOutputStream("lorem_testa_teclado.txt");
+		OutputStream fos = System.out;
+		Writer osw = new OutputStreamWriter(fos);
+		BufferedWriter bw = new BufferedWriter(osw);
+		
+		String linha = br.readLine();
+		
+		while(linha != null && !linha.isEmpty()) {
+			bw.write(linha);
+			bw.newLine();
+			bw.flush();
+			linha = br.readLine();
+		}
+		
+		br.close();
+		bw.close();
+	}
+
+}
