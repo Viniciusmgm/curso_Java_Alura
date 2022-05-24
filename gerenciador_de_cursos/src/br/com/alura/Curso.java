bf7842c5e9892374cbc3bdf651738b34359cdc11
Viniciusmgm <vinicius.mirandagm@gmail.com>
2022-05-24 09:34:47 -0300

    default methods

--- gerenciador_de_cursos/src/br/com/alura/Curso.java
+++ gerenciador_de_cursos/src/br/com/alura/Curso.java
@@ -1,9 +1,12 @@
 package br.com.alura;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 public class Curso {
@@ -12,6 +15,7 @@ public class Curso {
 	private String intrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
 	private Set<Aluno> alunos = new HashSet<>();
+	private Map<Integer, Aluno> matriculaParaAluno = new HashMap<>();
 	
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -58,10 +62,18 @@ public class Curso {
 
 	public void matricula(Aluno aluno) {
 		this.alunos.add(aluno);
+		this.matriculaParaAluno.put(aluno.getNumeroMatricula(), aluno);
 	}
 
 	public boolean estaMatriculado(Aluno aluno) {
 		return this.alunos.contains(aluno);
+	}
+
+	public Aluno buscaMatriculado(int numeroMatricula) {
+		if(!matriculaParaAluno.containsKey(numeroMatricula)) {
+			throw new NoSuchElementException();
+		}
+		return matriculaParaAluno.get(numeroMatricula);
 	}
 	
 }
